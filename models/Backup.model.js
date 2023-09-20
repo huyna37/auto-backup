bcc9383966ceb36944bdbf0e698fae0cfbfa1434
huyna <huyna@tuivietnam.com>
2023-09-20 13:44:19 +0700

    Update

--- models/Backup.model.js
+++ models/Backup.model.js
@@ -2,12 +2,12 @@ const cron = require('node-cron');
 const { exec } = require('child_process');
 const path = require('path');
 const fs = require('fs');
-
+const { uploadBackupFolder } = require('./Update.model')
 
 const backup = async () => {
     try {
         const date = new Date().toISOString().replace(/:/g, '-'); // Remove colons from the date to use in the file name
-        const backupDir = path.join(__dirname, '../backups'); // Use path.join for cross-platform compatibility
+        const backupDir = path.join(__dirname, '../'); // Use path.join for cross-platform compatibility
 
         // Create a backup directory if it doesn't exist
         if (!fs.existsSync(backupDir)) {
@@ -18,7 +18,7 @@ const backup = async () => {
         await exec(`mongodump --uri "mongodb://103.252.137.158:27018/truyenvui" --out ${backupDir}`, {
             shell: '/bin/bash'
         });
-
+        await uploadBackupFolder();
         console.log('Backup completed successfully.');
 
     } catch (error) {
--- models/Update.model.js
+++ models/Update.model.js
@@ -11,62 +11,80 @@ const REDIRECT_URL = process.env.REDIRECT_URL;
 const REFRESH_TOEKN = process.env.REFRESH_TOEKN;
 
 const oauth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REFRESH_TOEKN, REDIRECT_URL);
-oauth2Client.setCredentials({refresh_token: REFRESH_TOEKN});
+oauth2Client.setCredentials({ refresh_token: REFRESH_TOEKN });
 
 const drive = google.drive({
-    version: 'v3',
-    auth : oauth2Client
+  version: 'v3',
+  auth: oauth2Client
 });
 
 module.exports = {
-    updateFile: async () => {
-        try{
-            const createFile = await drive.files.create({
-                requestBody: {
-                    name: 'backup',
-                    mimeType: 'image/jpg'
-                },
-                media: {
-                    mimeType: 'image/jpg',
-                    body: fs.createReadStream(path.join(__dirname, '../crp.jpg'))
-                }
-            })
-            console.log('upload done', createFile.data)
+  updateFile: async () => {
+    try {
+      const createFile = await drive.files.create({
+        requestBody: {
+          name: 'backup',
+          mimeType: 'image/jpg'
+        },
+        media: {
+          mimeType: 'image/jpg',
+          body: fs.createReadStream(path.join(__dirname, '../crp.jpg'))
         }
-        catch(error){
-            console.error(error);
-        }
-    },
-    deleteFile: async (fileId) => {
-        try{
-            const deleteData = await drive.files.delete({
-                fileId: fileId
-            })
-            console.log('upload done', deleteData.data)
-        }
-        catch(error){
-            console.error(error);
-        }
-    },
-    listFiles : async (name) => {
-        try {
-          const files = await drive.files.list({
-            pageSize: 10, // Số lượng tệp bạn muốn lấy
-            fields: 'nextPageToken, files(id, name)', // Thông tin bạn muốn lấy cho mỗi tệp
-            q: `name contains '${name}'`,
-          });
-      
-          const fileList = files.data.files;
-          if (fileList.length > 0) {
-            console.log('Files:');
-            fileList.forEach((file) => {
-              console.log(`${file.name} (${file.id})`);
-            });
-          } else {
-            console.log('No files found.');
-          }
-        } catch (error) {
-          console.error('Error listing files:', error);
-        }
+      })
+      console.log('upload done', createFile.data)
+    }
+    catch (error) {
+      console.error(error);
+    }
+  },
+  deleteFile: async (fileId) => {
+    try {
+      const deleteData = await drive.files.delete({
+        fileId: fileId
+      })
+      console.log('upload done', deleteData.data)
+    }
+    catch (error) {
+      console.error(error);
+    }
+  },
+  uploadBackupFolder: async () => {
+    try {
+      // Tạo một thư mục mới trên Google Drive với tên và ngày tạo tương ứng
+      const currentDate = new Date().toISOString().replace(/:/g, '-');
+      const folderName = `Backup_${currentDate}`;
+      const folderMetadata = {
+        name: folderName,
+        mimeType: 'application/vnd.google-apps.folder',
+      };
+      const createFolder = await drive.files.create({
+        resource: folderMetadata,
+        fields: 'id',
+      });
+      const folderId = createFolder.data.id;
+
+      // Lấy danh sách tệp trong thư mục "backups" của bạn
+      const backupDir = path.join(__dirname, '../backups');
+      const backupFiles = fs.readdirSync(backupDir);
+
+      // Tải lên từng tệp vào thư mục mới trên Google Drive
+      for (const backupFile of backupFiles) {
+        const fileMetadata = {
+          name: backupFile,
+          parents: [folderId],
+        };
+        await drive.files.create({
+          resource: fileMetadata,
+          media: {
+            mimeType: 'application/octet-stream',
+            body: fs.createReadStream(path.join(backupDir, backupFile)),
+          },
+        });
       }
+
+      console.log(`Backup folder "${folderName}" uploaded to Google Drive.`);
+    } catch (error) {
+      console.error('Error uploading backup folder:', error);
+    }
+  },
 }
