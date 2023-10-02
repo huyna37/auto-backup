bef6ff7641b6b7ebd02378c899f8b7ba87f5f44e
huyna <huyna@tuivietnam.com>
2023-10-02 14:09:00 +0700

    Update backup token

--- models/Backup.model.js
+++ models/Backup.model.js
@@ -2,32 +2,42 @@ const { exec } = require('child_process');
 const path = require('path');
 const fs = require('fs');
 const { uploadBackupFolder } = require('./Update.model');
-const cron = require('node-cron');
 
 const backup = async () => {
     try {
         const backupDir = path.join(__dirname, '../'); // Use path.join for cross-platform compatibility
-
+        const backupDirE = path.join(__dirname, '../truyenvui');
         // Create a backup directory if it doesn't exist
-        if (!fs.existsSync(backupDir)) {
-            fs.mkdirSync(backupDir);
+        try {
+            if (fs.existsSync(backupDirE)) {
+                fs.rmSync(backupDirE, { recursive: true }); // Sử dụng tùy chọn { recursive: true } để xóa thư mục và nội dung bên trong (Node.js v12+)
+            }
+        } catch (err) {
+            // Xảy ra lỗi khi xóa thư mục, bạn có thể xử lý nó ở đây
+            console.error(`Error deleting backup directory: ${err.message}`);
         }
+        if (!fs.existsSync(backupDirE)) {
+            fs.mkdirSync(backupDirE);
+        }
+
 
         // Execute the mongodump command to backup the MongoDB database
         exec(`mongodump --uri=mongodb://103.252.137.158:27018/truyenvui --out ${backupDir}`).on('exit', (code, signal) => {
             if (code === 0) {
                 // Backup was successful, you can now proceed with the upload
-                uploadBackupFolder()
-                    .then(() => {
-                        console.log('Backup completed successfully.');
-                    })
-                    .catch((uploadError) => {
-                        console.error(`Upload failed: ${uploadError.message}`);
-                    });
+
             } else {
                 console.error(`mongodump process exited with code ${code} and signal ${signal}`);
             }
         });
+
+        uploadBackupFolder()
+            .then(() => {
+                console.log('Backup completed successfully.');
+            })
+            .catch((uploadError) => {
+                console.error(`Upload failed: ${uploadError.message}`);
+            });
 
     } catch (error) {
         console.error(`Backup process failed: ${error.message}`);
@@ -35,6 +45,5 @@ const backup = async () => {
 };
 
 // Run the backup function immediately on startup
-cron.schedule('0 0 * * *', () => {
-  backup();
-});
+
+module.exports = backup;
--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const app = express();
+const cron = require('node-cron');
 
-require('./models/Backup.model.js');
 
+const backup = require('./models/Backup.model.js');
+
+app.use('/backup', async (req, res) => {
+    await backup();
+    res.status(200).json('ok')
+})
+
+cron.schedule('0 0 * * *', () => {
+    backup();
+});
 app.listen(3100, () => {
     console.log(`Server is running on port ${3100}`);
 });
